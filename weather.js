576d0d2bf2ec7f1b2ee89c2d42b049e9a48486bd
gbz3 <1808483+gbz3@users.noreply.github.com>
2023-12-23 10:08:22 +0900

    json

--- weather.js
+++ weather.js
@@ -1,9 +1,14 @@
+const city = 'Nagoya';
+const api_url = `https://wttr.in/${city}?format=j1`;
+
 async function getWeather() {
     try {
-        const response = await fetch('https://wttr.in/Nagoya?format=%C+%t');
-        const weatherData = await response.text();
+        const response = await fetch(api_url);
+        const weather_json = await response.json();
+        const temp = weather_json.current_condition[0].temp_C;
 
-        console.log(`Current weather in Nagoya: ${weatherData}`);
+        console.log(`Current weather in Nagoya: ${weather_json.current_condition[0].weatherDesc[0].value}`);
+        console.log(`Current temperature in Nagoya: ${temp}°C`);
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
